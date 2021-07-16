01bd82c0ad2d60fa82af06e67a62d595daa9bc7e
Manav Parikh <manavparikhnotofficial@gmail.com>
2021-07-16 21:10:14 +0530

    Time: 1 ms (84.16%), Space: 39.5 MB (80.33%) - LeetHub

---
+++ concatenation-of-array/concatenation-of-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int len = nums.length;
+        int[] ans = new int[len*2];
+        for (int i=0; i<len*2;i++)
+        {
+            if(i<len)
+            {
+                ans[i]=nums[i];
+            }
+            else
+            {
+                ans[i]=nums[i-len];
+            }
+        }
+        return ans;
+    }
+}
