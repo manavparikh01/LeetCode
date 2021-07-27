d23349fdac5df003b69534932b218990a4f73b0f
Manav Parikh <manavparikhnotofficial@gmail.com>
2021-07-27 12:15:46 +0530

    Time: 28 ms (6.91%), Space: 39.1 MB (23.43%) - LeetHub

---
+++ truncate-sentence/truncate-sentence.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        String su="";
+        int sum=0;
+        int index=0;
+        for (int i=0;i<s.length();i++)
+        {
+            if(sum<k)
+            {
+                
+                if (Character.isWhitespace(s.charAt(i)))
+                {
+                    sum++;
+                    index = i;
+                    
+                }
+                System.out.println(sum);
+                System.out.println(index);
+            }
+            
+           
+            
+        }
+        if (sum<k)
+        {
+            su=s;
+            return su;
+        }
+        su = s.substring(0,index);
+        return su;
+    }
+}
