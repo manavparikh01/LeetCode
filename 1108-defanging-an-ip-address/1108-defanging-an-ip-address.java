88a205e695e5efa67d19f2d35c9d8f4375580bd8
Manav Parikh <manavparikhnotofficial@gmail.com>
2023-02-10 21:32:35 +0530

    Time: 6 ms (15.51%), Space: 40.8 MB (28.05%) - LeetHub

---
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String defangIPaddr(String address) {
+        String s = "";
+        for (int i=0;i<address.length();i++)
+        {
+            if (address.charAt(i) != '.')
+            {
+                s = s + address.charAt(i);
+            }
+            else
+            {
+                s = s + "[" + address.charAt(i) + "]";
+            }
+        }
+        return s;
+    }
+}
