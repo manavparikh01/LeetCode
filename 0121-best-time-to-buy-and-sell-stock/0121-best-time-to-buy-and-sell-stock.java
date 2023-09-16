5d62823f88d2430b3fc53a24afac005e67a39614
Manav Parikh <manavparikhnotofficial@gmail.com>
2023-09-16 17:23:25 -0400

    Time: 1 ms (99.95%), Space: 60.9 MB (67.25%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -4,28 +4,46 @@ class Solution {
         {
             return 0;
         }
-        int arr[] = new int[prices.length];
-        int max = 0;
-        int maxsell = prices[prices.length - 1];
-        arr[prices.length - 1] = maxsell;
-        for (int i = prices.length - 2;i>=0;i--)
-        {
-            if (prices[i] > maxsell)
-            {
-                maxsell = prices[i];
-            }
-            arr[i] = maxsell;
-        }
+//         int arr[] = new int[prices.length];
+//         int max = 0;
+//         int maxsell = prices[prices.length - 1];
+//         arr[prices.length - 1] = maxsell;
+//         for (int i = prices.length - 2;i>=0;i--)
+//         {
+//             if (prices[i] > maxsell)
+//             {
+//                 maxsell = prices[i];
+//             }
+//             arr[i] = maxsell;
+//         }
         
-        for (int i = 0;i<prices.length;i++)
+//         for (int i = 0;i<prices.length;i++)
+//         {
+//             int diff = arr[i] - prices[i];
+        //     max = Math.max(max, diff);
+        // }
+        // if (max <= 0)
+        // {
+        //     return 0;
+        // }
+        // return max;
+        int l = 0;
+        int r = 1;
+        int profit = 0;
+        while (r < prices.length)
         {
-            int diff = arr[i] - prices[i];
-            max = Math.max(max, diff);
+            if (prices[l] > prices[r])
+            {
+                l++;
+                r = l + 1;
+            }
+            else
+            {
+                int pro = prices[r] - prices[l];
+                profit = Math.max(profit, pro);
+                r++;
+            }
         }
-        if (max <= 0)
-        {
-            return 0;
-        }
-        return max;
+        return profit;
     }
 }
