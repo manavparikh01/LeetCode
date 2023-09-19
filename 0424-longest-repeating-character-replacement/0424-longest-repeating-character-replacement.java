9b02d1bd7b4540c17551333df7e92c47f66f0093
Manav Parikh <manavparikhnotofficial@gmail.com>
2023-09-19 13:13:02 -0400

    Time: 23 ms (30.45%), Space: 44 MB (11.90%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -9,25 +9,18 @@ class Solution {
             if (!hm.containsKey(s.charAt(r)))
             {
                 hm.put(s.charAt(r), 1);
-                //System.out.println(s.charAt(r) + " " + hm.get(s.charAt(r)));
             }
             else
             {
                 hm.put(s.charAt(r), hm.get(s.charAt(r)) + 1);
-                //System.out.println(s.charAt(r) + " " + hm.get(s.charAt(r)));
                 max = Math.max(max, hm.get(s.charAt(r)));
             }
-            if ((r-l+1) - max <= k)
-            {
-                res = Math.max(res, (r-l+1));
-                
-            }
-            else
+            while ((r-l+1) - max > k)
             {
                 hm.put(s.charAt(l), hm.get(s.charAt(l)) - 1);
                 l++;
             }
-            
+            res = Math.max(res, (r-l+1));
         }
         return res;
     }
