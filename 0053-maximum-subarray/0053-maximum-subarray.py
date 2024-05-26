5e61c0b9d8c5cd3b132a3979c0eef2e56c4bcf88
Manav Parikh <manavparikhnotofficial@gmail.com>
2024-05-26 14:31:43 -0400

    Time: 556 ms (24.39%), Space: 30.9 MB (63.49%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -5,5 +5,4 @@ class Solution:
         for i in range(1, len(nums), 1):
             sum1 = max(sum1+nums[i], nums[i])
             max1 = max(max1, sum1)
-            print("sum", sum1)
         return max1
