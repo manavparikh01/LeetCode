4fb6fc579e19996f1b7a5eb52f2f7547df98b91e
Manav Parikh <manavparikhnotofficial@gmail.com>
2024-06-20 00:33:05 -0400

    Time: 627 ms (83.10%), Space: 0B (100.00%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,20 +1,16 @@
 # Write your MySQL query statement below
-WITH DateActivity AS (SELECT player_id, MIN(event_date) as event_date
+WITH DateActivity AS (SELECT player_id, MIN(event_date) AS event_date
 FROM Activity
 GROUP BY player_id),
-NotThatDate AS (SELECT a.player_id, a.event_date
+NotThatDate AS (SELECT a.player_id, MIN(a.event_date) AS event_date
 FROM Activity AS a
 JOIN DateActivity AS d
-ON a.player_id = d.player_id AND a.event_date != d.event_date),
-FinalDate AS (SELECT player_id, MIN(event_date) as event_date
-FROM NotThatDate
-GROUP BY player_id),
+ON a.player_id = d.player_id AND a.event_date != d.event_date
+GROUP BY a.player_id),
 ConsecutiveDate AS (SELECT f.player_id
-FROM FinalDate AS f
+FROM NotThatDate AS f
 JOIN DateActivity AS d
 ON f.player_id = d.player_id
 WHERE f.event_date > d.event_date AND DATEDIFF(f.event_date, d.event_date) = 1)
 SELECT ROUND((SELECT COUNT(player_id) FROM ConsecutiveDate) / COUNT(DISTINCT player_id), 2) AS fraction
 FROM Activity;
--- SELECT ROUND(SELECT COUNT(player_id) FROM ConsecutiveDate/COUNT(DISTINCT player_id),2) AS fraction
--- FROM Actitvity;
