6b48d5f4d51d784a744e92f8aa445e854f7bc0a9
Manav Parikh <manavparikhnotofficial@gmail.com>
2024-06-21 12:35:46 -0400

    Time: 376 ms (87.58%), Space: 0B (100.00%) - LeetHub

---
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+WITH SumNumber AS (SELECT num, COUNT(num) as countn
+                  FROM MyNumbers
+                  GROUP BY num
+                  ORDER BY num DESC)
+SELECT MAX(num) AS num
+FROM MyNumbers
+WHERE num NOT IN (SELECT num
+                 FROM SumNumber
+                 WHERE countn > 1);
