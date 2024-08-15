7f74e7f2cccde1cb0db5c92917d70b946202e89e
Manav Parikh <manavparikh01@gmail.com>
2024-08-14 20:17:00 -0400

    Time: 55 ms (72.30%), Space: 16.7 MB (48.53%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minPatches(self, nums: List[int], n: int) -> int:
+        right = 0
+        count = 0
+        for x in nums:
+            while n > right and x > right + 1:
+                right += right + 1
+                count += 1
+            right += x
+        while n > right:
+            right += right + 1
+            count += 1
+        return count
