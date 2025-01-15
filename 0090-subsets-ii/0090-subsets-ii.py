319a649d8140989ab90adc2f6fecfb4e54cf60c5
Manav Parikh <manavparikh01@gmail.com>
2025-01-15 17:28:37 +0530

    Time: 7 ms (16.52%), Space: 12.5 MB (45.38%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -15,6 +15,8 @@ class Solution(object):
             self.slist.append(nums[i])
             dfs(i+1)
             self.slist.pop()
+            while (i+1 < len(nums) and nums[i] == nums[i+1]):
+                i = i+1
             dfs(i+1)
         dfs(0)
         return self.mainlist
