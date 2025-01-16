e5b827fe27e90114a7fe5e1da5d25f50afdb6e89
Manav Parikh <manavparikh01@gmail.com>
2025-01-16 20:32:15 +0530

    Time: 19 ms (62.95%), Space: 12.7 MB (11.34%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        ans = []
+        ds = []
+        def findCombination(ind, target):
+            if ind == len(candidates):
+                if target == 0:
+                    ans.append(ds[:])
+                return
+            if candidates[ind] <= target:
+                ds.append(candidates[ind])
+                findCombination(ind, target - candidates[ind])
+                ds.pop()
+            findCombination(ind + 1, target)
+        findCombination(0, target)
+        return ans
+        
