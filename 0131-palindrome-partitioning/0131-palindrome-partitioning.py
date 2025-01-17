5e132e6fb62ca777c1102484bbef32db852bfc1f
Manav Parikh <manavparikh01@gmail.com>
2025-01-17 23:53:08 +0530

    Time: 83 ms (58.83%), Space: 47.8 MB (65.25%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def partition(self, s):
+        """
+        :type s: str
+        :rtype: List[List[str]]
+        """
+        res = []
+        path = []
+        def partitionHelper(index):
+            if index == len(s):
+                res.append(path[:])
+                return
+            for i in range(index, len(s)):
+                if isPalindrome(s, index, i):
+                    path.append(s[index:i + 1])
+                    partitionHelper(i + 1)
+                    path.pop()
+
+
+        def isPalindrome(s, start, end):
+            while start <= end:
+                if s[start] != s[end]:
+                    return False
+                start += 1
+                end -= 1
+            return True
+
+        partitionHelper(0)
+        return res
