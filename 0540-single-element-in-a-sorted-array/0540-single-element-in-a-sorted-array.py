e371a74b5db2f97d3edd7c051a121014ac840b32
Manav Parikh <manavparikh01@gmail.com>
2025-01-22 01:27:13 +0530

    Time: 0 ms (100%), Space: 16.4 MB (67.02%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -8,23 +8,18 @@ class Solution(object):
         h = len(nums) - 1
         while l <= h:
             mid = (l+h)//2
-            print(mid)
             if (mid - 0 + 1) % 2 == 1:
                 if mid - 1 >=0 and nums[mid - 1] == nums[mid]:
                     h = mid - 1
-                    print(h, "helo 1")
                 elif mid + 1 < len(nums) and nums[mid + 1] == nums[mid]:
                     l = mid + 1
-                    print(l, "helo 2")
                 else:
                     return nums[mid]
             else:
                 if mid - 1 >= 0 and nums[mid - 1] == nums[mid]:
                     l = mid + 1
-                    print(l, "helo 3")
                 elif mid + 1 < len(nums) and nums[mid + 1] == nums[mid]:
                     h = mid - 1
-                    print(h, "helo 4")
                 else:
                     return nums[mid]
         return -1
