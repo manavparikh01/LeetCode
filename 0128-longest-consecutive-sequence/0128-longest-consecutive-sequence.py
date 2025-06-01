721c2b6126443bada6d49eda03212ad14e0d838f
Manav Parikh <manavparikh01@gmail.com>
2025-06-01 01:12:17 -0400

    Time: 53 ms (63.63%), Space: 27.1 MB (45.41%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,56 +1,19 @@
-class Solution:
-    def longestConsecutive(self, nums: List[int]) -> int:
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
         if len(nums) == 0:
             return 0
-        nums.sort()
-        maxq = 1;
-        j = 0
-        small = float('-inf')
-        for i in range(len(nums)):
-            if nums[i] - 1 == small:
-                j = j+1
-                small = nums[i]
-                
-            elif nums[i] != small:
-                small = nums[i]
-                j = 1
-            maxq = max(maxq,j)
-        return maxq
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # if len(nums) == 0:
-        #     return 0
-        # if min(nums) < 0:
-        #     list = [0] * (max(nums) - min(nums) + 1)
-        #     for i in nums:
-        #         i = i - min(nums)
-        #         list[i] = list[i] + 1
-        # else:
-        #     list = [0] * (max(nums) + 1)
-        #     for i in nums:
-        #         list[i] = list[i] + 1
-        # i = 1
-        # j = 1
-        # maxi = 1
-        # while i < len(list):
-        #     while i < len(list) and list[i] > 0 and list[i-1] > 0:
-        #         j = j + 1
-        #         print(i, j)
-        #         i = i + 1
-        #     maxi = max(j, maxi)
-        #     i = i + 1
-        #     j = 1
-        # return maxi
+        numsu = set(nums)
+        length = 1
+        for i in numsu:
+            slength = 1
+            if i - 1 in numsu:
+                continue
+            while i + 1 in numsu:
+                slength += 1
+                i = i+1
+            length = max(length, slength)
+        return length
