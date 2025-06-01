645943452950091c3a7839337bd99065adc5fe7f
Manav Parikh <manavparikh01@gmail.com>
2025-05-31 23:55:12 -0400

    Time: 23 ms (96.89%), Space: 20.4 MB (48.96%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -4,21 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        pro = 1
-        zero = 0
-        zeroindex = []
+        res = [1] * len(nums)
+        prefix = 1
         for i in range(0, len(nums)):
-            if nums[i] is not 0:
-                pro *= nums[i]
-            else:
-                zeroindex.append(i)
-                zero += 1
-        res = [0] * len(nums)
-        if zero > 1:
-            return res
-        if zero == 1:
-            res[zeroindex[0]] = pro
-            return res
-        for i in range(0, len(nums)):
-            res[i] = pro//nums[i]
-        return res
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res 
