c13965a1a8fba5abb9ac0942e711b2e30f5ff605
Manav Parikh <manavparikh01@gmail.com>
2025-06-12 12:34:13 -0400

    Time: 0 ms (100%), Space: 12.6 MB (46.33%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -1,35 +1,26 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if list1 == None:
-            return list2
-        if list2 == None:
-            return list1
-        curr = list1
-        curr2 = list2
-        if curr.val <= curr2.val:
-            temp = curr
-            head = temp
-            curr = curr.next
-        else:
-            temp = curr2
-            head = temp
-            curr2 = curr2.next
-        while curr != None and curr2 != None:
-            if curr.val <= curr2.val:
-                temp.next = curr
-                temp = curr
-                curr = curr.next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy = node = ListNode()
+        while list1 and list2:
+            if list1.val < list2.val:
+                node.next = list1
+                list1 = list1.next
             else:
-                temp.next = curr2
-                temp = curr2
-                curr2 = curr2.next
-        if curr != None:
-            temp.next = curr
-        if curr2 != None:
-            temp.next = curr2
-        return head
+                node.next = list2
+                list2 = list2.next
+            node = node.next
+        if list1 == None:
+            node.next = list2
+        else:
+            node.next = list1
+        return dummy.next
