de598c4b8516509db38886e43aa3d003ee02ece9
Manav Parikh <manavparikh01@gmail.com>
2025-06-27 19:18:33 -0400

    Time: 125 ms (32.93%), Space: 18.3 MB (45.46%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -6,19 +6,29 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         min_heap = []
-        hash_map = {}
         res = []
         for point in points:
             temp_val = point[0]**2 + point[1]**2
-            heapq.heappush(min_heap, -temp_val)
-            if temp_val not in hash_map:
-                hash_map[temp_val] = [point]
-            else:
-                hash_map[temp_val].append(point)
+            heapq.heappush(min_heap, [-temp_val, point[0], point[1]])
             if len(min_heap) > k:
-                pop_val = -heapq.heappop(min_heap)
-                del hash_map[pop_val][0]
-        for point in hash_map.values():
-            for arr in point:
-                res.append(arr)
+                heapq.heappop(min_heap)
+        for point in min_heap:
+            res.append([point[1], point[2]])
         return res
+        # min_heap = []
+        # hash_map = {}
+        # res = []
+        # for point in points:
+        #     temp_val = point[0]**2 + point[1]**2
+        #     heapq.heappush(min_heap, -temp_val)
+        #     if temp_val not in hash_map:
+        #         hash_map[temp_val] = [point]
+        #     else:
+        #         hash_map[temp_val].append(point)
+        #     if len(min_heap) > k:
+        #         pop_val = -heapq.heappop(min_heap)
+        #         del hash_map[pop_val][0]
+        # for point in hash_map.values():
+        #     for arr in point:
+        #         res.append(arr)
+        # return res
