facf6a17f967ee8e36bd73cbce22952bd8621a2e
Manav Parikh <manavparikh01@gmail.com>
2025-07-10 21:20:23 -0400

    Time: 56 ms (30.97%), Space: 34.4 MB (80.67%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,29 +1,34 @@
-class Solution(object):
-    def partition(self, s):
-        """
-        :type s: str
-        :rtype: List[List[str]]
-        """
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
         res = []
-        path = []
-        def partitionHelper(index):
-            if index == len(s):
-                res.append(path[:])
+        temp = []
+        def dp(i):
+            if i >= len(s):
+                res.append(temp.copy())
                 return
-            for i in range(index, len(s)):
-                if isPalindrome(s, index, i):
-                    path.append(s[index:i + 1])
-                    partitionHelper(i + 1)
-                    path.pop()
-
-
-        def isPalindrome(s, start, end):
-            while start <= end:
-                if s[start] != s[end]:
-                    return False
-                start += 1
-                end -= 1
-            return True
-
-        partitionHelper(0)
+            for j in range(i, len(s)):
+                if s[i:j+1] == s[i:j+1][::-1]:
+                    temp.append(s[i:j+1])
+                    dp(j+1)
+                    temp.pop()
+            return
+        dp(0)
         return res
+        # res = []
+        # temp = []
+        # def dp(l, r):
+        #     nonlocal res, temp
+        #     print(l, r)
+        #     if l == r:
+        #         return
+        #     if s[l:r] == s[l:r][::-1]:
+        #         temp.append(s[l:r])
+        #     for i in range(l + 1, r):
+        #         dp(l, i)
+        #         dp(i, r)
+        #     res.append(temp.copy())
+        #     return
+        
+        # dp(0, len(s))
+        # return res
+            
