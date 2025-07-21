2b7b527d8d671884caceed76f509a039a1675f9e
Manav Parikh <manavparikh01@gmail.com>
2025-07-20 20:30:35 -0400

    Time: 6743 ms (5.06%), Space: 19.5 MB (35.53%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        hashmap = {}
+        visited = set()
+        res = []
+
+        for depends in prerequisites:
+            if depends[0] in hashmap:
+                hashmap[depends[0]].append(depends[1])
+            else:
+                hashmap[depends[0]] = [depends[1]]
+    
+        
+        def ifPossible(i):
+            if i not in hashmap:
+                if i not in res:
+                    res.append(i)
+                return True
+            if i in visited:
+                return False
+            visited.add(i)
+            for idx in hashmap[i]:
+                if ifPossible(idx) == False:
+                    return False
+            visited.remove(i)
+            if i not in res:
+                res.append(i)
+            return True
+
+        for i in range(numCourses):
+            if ifPossible(i) == False:
+                return []
+                
+        return res
