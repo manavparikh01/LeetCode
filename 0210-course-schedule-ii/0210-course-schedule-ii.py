05b1e7e4dc5e4c06b1c8906b5e0187dcdc3251e8
Manav Parikh <manavparikh01@gmail.com>
2025-07-20 20:39:57 -0400

    Time: 6649 ms (5.06%), Space: 19.5 MB (35.53%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,60 +1,36 @@
+from collections import defaultdict
+
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        prereq = {c: [] for c in range(numCourses)}
-        for crs, pre in prerequisites:
-            prereq[crs].append(pre)
+        hashmap = defaultdict(list)
+        visited = set()
+        res = []
 
-        output = []
-        visit, cycle = set(), set()
-
-        def dfs(crs):
-            if crs in cycle:
-                return False
-            if crs in visit:
-                return True
-
-            cycle.add(crs)
-            for pre in prereq[crs]:
-                if dfs(pre) == False:
-                    return False
-            cycle.remove(crs)
-            visit.add(crs)
-            output.append(crs)
-            return True
-
-        for c in range(numCourses):
-            if dfs(c) == False:
-                return []
-        return output
-        # hashmap = {}
-        # visited = set()
-        # res = []
-
-        # for depends in prerequisites:
-        #     if depends[0] in hashmap:
-        #         hashmap[depends[0]].append(depends[1])
-        #     else:
-        #         hashmap[depends[0]] = [depends[1]]
+        for depends in prerequisites:
+            if depends[0] in hashmap:
+                hashmap[depends[0]].append(depends[1])
+            else:
+                hashmap[depends[0]] = [depends[1]]
     
         
-        # def ifPossible(i):
-        #     if i not in hashmap:
-        #         if i not in res:
-        #             res.append(i)
-        #         return True
-        #     if i in visited:
-        #         return False
-        #     visited.add(i)
-        #     for idx in hashmap[i]:
-        #         if ifPossible(idx) == False:
-        #             return False
-        #     visited.remove(i)
-        #     if i not in res:
-        #         res.append(i)
-        #     return True
+        def ifPossible(i):
+            if i not in hashmap:
+                if i not in res:
+                    res.append(i)
+                return True
+            if i in visited:
+                return False
+            visited.add(i)
+            for idx in hashmap[i]:
+                if ifPossible(idx) == False:
+                    return False
+            visited.remove(i)
+            if i not in res:
+                res.append(i)
+            return True
 
-        # for i in range(numCourses):
-        #     if ifPossible(i) == False:
-        #         return []
+        for i in range(numCourses):
+            if ifPossible(i) == False:
+                return []
                 
-        # return res
+        return res
