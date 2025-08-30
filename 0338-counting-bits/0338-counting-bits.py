540bf2aa636a149ac093fc4b3dcb700397a880aa
Manav Parikh <manavparikh01@gmail.com>
2025-08-30 14:07:14 -0400

    Time: 32 ms (23.34%), Space: 18.5 MB (90.37%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -2,5 +2,13 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
         array = [0] * (n+1)
         for i in range(n + 1):
-            array[i] = bin(i).count("1")
+            res = 0
+            if i == 0:
+                array[i] = 0
+                continue
+            itemp = i
+            while itemp:
+                itemp &= (itemp - 1)
+                res += 1
+            array[i] = res
         return array
