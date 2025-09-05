dd633783157d9d125cb1e61ef2780242b281cbb8
Manav Parikh <manavparikh01@gmail.com>
2025-09-05 17:48:55 -0400

    Time: 175 ms (20.1%), Space: 77.9 MB (91.92%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,26 +1,48 @@
+class Node:
+
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.queue = deque()
         self.capacity = capacity
         self.hashmap = defaultdict(int)
+        self.left, self.right = Node(0, 0), Node(0, 0)
+        self.left.next, self.right.prev = self.right, self.left
+    
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
+
+    def insert(self, node):
+        nlru = self.right.prev
+        nlru.next = node
+        self.right.prev = node
+        node.next = self.right
+        node.prev = nlru
+
 
     def get(self, key: int) -> int:
         if key in self.hashmap:
-            self.queue.remove(key)
-            self.queue.append(key)
-            return self.hashmap[key]
+            self.remove(self.hashmap[key])
+            self.insert(self.hashmap[key])
+            return self.hashmap[key].value
         return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.hashmap:
-            temp = self.get(key)
-        else:
-            if len(self.queue) == self.capacity:
-                temp = self.queue.popleft()
-                del self.hashmap[temp]
-            self.queue.append(key)
-        self.hashmap[key] = value
+            self.remove(self.hashmap[key])
+        self.hashmap[key] = Node(key, value)
+        self.insert(self.hashmap[key])
+        if len(self.hashmap) > self.capacity:
+            lru = self.left.next
+            self.remove(lru)
+            del self.hashmap[lru.key]
         # print(self.queue, self.hashmap)
 
 
