074826bfe82093819e909a50674c5d19e2e91766
Manav Parikh <manavparikh01@gmail.com>
2025-09-24 23:30:42 -0400

    Time: 31 ms (96.14%), Space: 17.6 MB (81.74%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.py
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.py
@@ -4,13 +4,16 @@ class Codec:
         self.length = 1
         self.encodemap = {}
         self.decodemap = {}
+        self.base = "https://tinyurl.com/"
 
     def encode(self, longUrl: str) -> str:
         """Encodes a URL to a shortened URL.
         """
-        self.encodemap[longUrl] = self.length
-        self.decodemap[self.length] = longUrl
-        return self.length
+        if longUrl not in self.encodemap:
+            shorturl = self.base + str(self.length)
+            self.encodemap[longUrl] = shorturl
+            self.decodemap[shorturl] = longUrl
+        return self.encodemap[longUrl]
         
 
     def decode(self, shortUrl: str) -> str:
