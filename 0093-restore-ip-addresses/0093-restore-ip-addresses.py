6605dafebcdade98c673b154129cc91cd400b414
Manav Parikh <manavparikh01@gmail.com>
2025-10-04 20:50:32 -0400

    Time: 3 ms (58.58%), Space: 17.8 MB (48.22%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -0,0 +1,33 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        arr = []
+        temp = []
+        def dfs(i, s):
+            nonlocal arr, temp
+            if len(temp) > 0:
+                if int(temp[-1]) > 255:
+                    return
+            if len(temp) == 4 and i == len(s):
+                arr.append(".".join(temp))
+            if len(temp) == 4 and i < len(s):
+                return
+            if i >= len(s):
+                return
+            
+            ip = s[i]
+            temp.append(ip)
+            dfs(i + 1, s)
+            temp.pop()
+            if s[i] != "0":
+                if i + 1 < len(s):
+                    ip = s[i:i+2]
+                    temp.append(ip)
+                    dfs(i + 2, s)
+                    temp.pop()
+                if i + 2 < len(s):
+                    ip = s[i:i+3]
+                    temp.append(ip)
+                    dfs(i + 3, s)
+                    temp.pop()
+        dfs(0, s)
+        return arr
