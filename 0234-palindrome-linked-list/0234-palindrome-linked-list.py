49006b60bc2953dd800b6c689529dee9b8e6bed1
Manav Parikh <manavparikh01@gmail.com>
2025-10-16 17:19:06 -0400

    Time: 34 ms (52.96%), Space: 34.8 MB (63.08%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,27 +5,21 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        if (head == None or head.next == None):
-            return True
-        curr = head
-        curr2 = head
-        curr3 = head
-        while curr2.next != None and curr2.next.next != None:
+        curr = dcurr = head
+        while dcurr.next and dcurr.next.next:
             curr = curr.next
-            curr2 = curr2.next.next
+            dcurr = dcurr.next.next
         prev = None
-        curr4 = curr.next
-        while curr4:
-            temp = curr4.next
-            curr4.next = prev
-            prev = curr4
-            curr4 = temp
-        #print(prev.val, curr3.val)
-        while prev:
-            #print(prev.val, curr3.val, "Hello")
-            if curr3.val != prev.val:
+        ncurr = curr.next
+        while ncurr:
+            temp = ncurr.next
+            ncurr.next = prev
+            prev = ncurr
+            ncurr = temp
+        mcurr = head
+        while mcurr and prev:
+            if mcurr.val != prev.val:
                 return False
-            curr3 = curr3.next
+            mcurr = mcurr.next
             prev = prev.next
         return True
-            
