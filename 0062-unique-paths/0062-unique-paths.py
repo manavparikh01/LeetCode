ac259f8bee6436c8e57db253e224ed96015833fa
Manav Parikh <manavparikh01@gmail.com>
2025-10-17 15:51:22 -0400

    Time: 0 ms (100%), Space: 17.7 MB (88.67%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,23 +1,16 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        i=0
-        j=0
-        total = 0
-        arr = [[-1 for j in range(n)] for i in range(m)]
-        def df(i, j, total, arr):
-            if arr[i][j] != -1:
-                return arr[i][j]
-            if i == m-1 or j == n-1:
-                total = total + 1
-                return total
-            temp1 = df(i+1, j, total, arr)
-            temp2 = df(i, j+1, total, arr)
-            arr[i][j] = temp1 + temp2
-            return arr[i][j]
-        total = df(0,0,0, arr)
-        # arr[m][n] = 0
-        # total = df(0,0, 0)
-        # for i in range(0,m):
-        #     for j in range(0,n):
-                
-        return total
+        arr = [[0 for i in range(n)] for j in range(m)]
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                left, down = -1, -1
+                if j == n - 1:
+                    left = 0
+                else:
+                    left = arr[i][j + 1]
+                if i == m - 1:
+                    down = 0
+                else:
+                    down = arr[i + 1][j]
+                arr[i][j] = max(1, left + down)
+        return arr[0][0]
