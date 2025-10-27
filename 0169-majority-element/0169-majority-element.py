b6b80b92b6bba331820b87386efc8ed6265a00fc
Manav Parikh <manavparikh01@gmail.com>
2025-10-27 00:18:12 -0400

    Time: 0 ms (100%), Space: 19.3 MB (87.01%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,14 +1,4 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        map = {}
-        length = len(nums)
-        if length < 3:
-            return nums[0]
-        for i in nums:
-            if i in map:
-                map[i] = map[i]+1
-                if map[i] > length/2:
-                    return i
-            else:
-                map[i] = 1
-        return -1
+        nums.sort()
+        return nums[len(nums)//2]
