d7fe234f516c9223a14d8e4be749b7afe23c6e88
Manav Parikh <manavparikh01@gmail.com>
2025-11-02 22:12:54 -0500

    Time: 22 ms (66.07%), Space: 19.6 MB (30.04%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -4,16 +4,13 @@ class Solution:
         arr = nums.copy() + nums.copy()
         finarr = arr.copy()
         for i in range(len(arr) - 1, -1, -1):
-            temp = []
             while stack and stack[-1] <= arr[i]:
-                temp.append(stack.pop())
+                stack.pop()
             if stack:
                 if stack[-1] > arr[i]:
                     finarr[i] = stack[-1]
             else:
                 finarr[i] = -1
-            while temp:
-                stack.append(temp.pop())
             stack.append(arr[i])
         # for i in range(len(nums) - 1, -1, -1):
         #     if arr[i] != nums[i]:
