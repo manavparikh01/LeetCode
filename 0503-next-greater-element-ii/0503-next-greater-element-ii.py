e30f708497b20cd1fd80b05a1360934ea5e2316c
Manav Parikh <manavparikh01@gmail.com>
2025-11-02 22:04:48 -0500

    Time: 7048 ms (5.01%), Space: 20.6 MB (7.39%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -1,29 +1,32 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
         stack = []
-        arr = nums.copy()
-        for i in range(len(nums) - 1, -1, -1):
+        arr = nums.copy() + nums.copy()
+        finarr = arr.copy()
+        for i in range(len(arr) - 1, -1, -1):
             temp = []
-            while stack and stack[-1] <= nums[i]:
+            while stack and stack[-1] <= arr[i]:
                 temp.append(stack.pop())
             if stack:
-                if stack[-1] > nums[i]:
-                    arr[i] = stack[-1]
-            while temp:
-                stack.append(temp.pop())
-            stack.append(nums[i])
-        for i in range(len(nums) - 1, -1, -1):
-            if arr[i] != nums[i]:
-                continue
-            temp = []
-            while stack and stack[-1] <= nums[i]:
-                temp.append(stack.pop())
-            if stack:
-                if stack[-1] > nums[i]:
-                    arr[i] = stack[-1]
+                if stack[-1] > arr[i]:
+                    finarr[i] = stack[-1]
             else:
-                arr[i] = -1
+                finarr[i] = -1
             while temp:
                 stack.append(temp.pop())
-            stack.append(nums[i])
-        return arr
+            stack.append(arr[i])
+        # for i in range(len(nums) - 1, -1, -1):
+        #     if arr[i] != nums[i]:
+        #         continue
+        #     temp = []
+        #     while stack and stack[-1] <= nums[i]:
+        #         temp.append(stack.pop())
+        #     if stack:
+        #         if stack[-1] > nums[i]:
+        #             arr[i] = stack[-1]
+        #     else:
+        #         arr[i] = -1
+        #     while temp:
+        #         stack.append(temp.pop())
+        #     stack.append(nums[i])
+        return finarr[:len(nums)]
