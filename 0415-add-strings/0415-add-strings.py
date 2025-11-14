d355179698d213244a554db3aa9e98958b434fa0
Manav Parikh <manavparikh01@gmail.com>
2025-11-14 17:51:31 -0500

    Time: 19 ms (5.49%), Space: 18.3 MB (37.07%) - LeetHub

--- 0415-add-strings/0415-add-strings.py
+++ 0415-add-strings/0415-add-strings.py
@@ -4,25 +4,14 @@ class Solution:
         i = len(num1) - 1
         j = len(num2) - 1
         res = ""
-        while i >= 0 and j >= 0:
-            total = int(num1[i]) + int(num2[j]) + carry
+        while i >= 0 or j >= 0 or carry > 0:
+            numa = int(num1[i]) if i >= 0 else 0
+            numb = int(num2[j]) if j >= 0 else 0
+            print(numa, numb, carry)
+            total = numa + numb + carry
             digit = total % 10
             carry = total // 10
             res = str(digit) + res
             i -= 1
             j -= 1
-        while i >= 0:
-            total = int(num1[i]) + carry
-            digit = total % 10
-            carry = total // 10
-            res = str(digit) + res
-            i -= 1
-        while j >= 0:
-            total = int(num2[j]) + carry
-            digit = total % 10
-            carry = total // 10
-            res = str(digit) + res
-            j -= 1
-        if carry > 0:
-            res = str(carry) + res
         return res
