fb9f2c0e63b098b8d7542b14dcae0565c64e750f
Manav Parikh <manavparikh01@gmail.com>
2025-11-21 02:03:32 -0500

    Time: 24 ms (73.26%), Space: 29.3 MB (12.94%) - LeetHub

---
+++ 0932-monotonic-array/0932-monotonic-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        if len(nums) <= 2:
+            return True
+        isInc = False
+        diff = nums[-1] - nums[0]
+        if diff > 0:
+            for i in range(1, len(nums)):
+                if nums[i] - nums[i - 1] < 0:
+                    return False
+        elif diff < 0:
+            for i in range(1, len(nums)):
+                if nums[i] - nums[i - 1] > 0:
+                    return False
+        else:
+            for i in range(1, len(nums)):
+                if nums[i] - nums[i - 1] != 0:
+                    return False
+        return True
