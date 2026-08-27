4a58fc0e7e48912ffccac1bc118fcd8938256c32
Manav Parikh <manavparikh01@gmail.com>
2026-08-26 23:27:26 -0400

    Time: 27 ms (29.12%), Space: 22.1 MB (34.49%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -1,22 +1,13 @@
-class Solution(object):
-    def findContentChildren(self, g, s):
-        """
-        :type g: List[int]
-        :type s: List[int]
-        :rtype: int
-        """
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
         g.sort()
         s.sort()
-        leng = len(g)
-        lens = len(s)
         i = 0
-        j = 0
-        temp = 0
-        while i < leng and j < lens:
+        count = 0
+        for j in range(0, len(s)):
+            if i == len(g):
+                return count
             if s[j] >= g[i]:
-                i = i + 1
-                j = j + 1
-                temp = temp + 1
-            else:
-                j = j + 1
-        return temp
+                count += 1
+                i += 1
+        return count
