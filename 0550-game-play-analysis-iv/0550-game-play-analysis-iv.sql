a9d1cf88069b9a790994751c7c8dd54bad6c9d80
Manav Parikh <manavparikh01@gmail.com>
2026-08-30 23:24:06 -0400

    Time: 606 ms (56.92%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,18 +1,11 @@
 # Write your MySQL query statement below
-WITH DateActivity AS (SELECT player_id, MIN(event_date) AS event_date
-FROM Activity
+WITH logged_date AS (SELECT player_id, MIN(event_date) as min_event_date FROM Activity
 GROUP BY player_id),
-NotThatDate AS (SELECT a.player_id, a.event_date
-FROM Activity AS a
-JOIN DateActivity AS d
-ON a.player_id = d.player_id AND a.event_date != d.event_date),
-FinalDate AS (SELECT player_id, MIN(event_date) as event_date
-FROM NotThatDate
-GROUP BY player_id),
-ConsecutiveDate AS (SELECT f.player_id
-FROM FinalDate AS f
-JOIN DateActivity AS d
-ON f.player_id = d.player_id
-WHERE f.event_date > d.event_date AND DATEDIFF(f.event_date, d.event_date) = 1)
-SELECT ROUND((SELECT COUNT(player_id) FROM ConsecutiveDate) / COUNT(DISTINCT player_id), 2) AS fraction
-FROM Activity;
+not_loggin_date AS (SELECT a1.player_id, MIN(a1.event_date) as sec_event_date FROM Activity a1
+JOIN logged_date l1 ON l1.player_id = a1.player_id AND l1.min_event_date != a1.event_date
+GROUP BY a1.player_id),
+logged_again AS (SELECT l1.player_id FROM logged_date l1
+JOIN not_loggin_date as l2 ON l1.player_id = l2.player_id
+WHERE DATEDIFF(l2.sec_event_date, l1.min_event_date) = 1)
+-- SELECT COUNT(DISTINCT player_id) FROM Activity
+SELECT ROUND((SELECT COUNT(player_id) from logged_again) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
