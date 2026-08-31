59a00df1ef5d857374c11a9bc68a3f2c6830359c
Manav Parikh <manavparikh01@gmail.com>
2026-08-31 15:57:53 -0400

    Time: 5 ms (87.74%), Space: 20.6 MB (21.4%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,7 +1,10 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        string = ""
-        for schar in s:
-            if schar.isalnum():
-                string += schar.lower()
-        return string == string[::-1]
+        se = re.sub(r'[^a-z0-9]', '', s.lower())
+        l, h = 0, len(se) - 1
+        while l < h:
+            if se[l] != se[h]:
+                return False
+            l += 1
+            h -= 1
+        return True
