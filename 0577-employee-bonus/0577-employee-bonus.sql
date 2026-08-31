5df5097beb0dbd8b7ef6fcf49ef184d066913db9
Manav Parikh <manavparikh01@gmail.com>
2026-08-30 22:48:54 -0400

    Time: 1024 ms (69.96%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
-SELECT e.name, b.bonus
-FROM Employee AS e
-LEFT JOIN Bonus AS b
+SELECT e.name, b.bonus FROM Employee e
+LEFT JOIN Bonus b
 ON e.empId = b.empId
-WHERE e.empId not in (SELECT empId FROM Bonus WHERE bonus >= 1000)
+WHERE e.empId NOT IN (SELECT empId FROM Bonus WHERE bonus >= 1000)
