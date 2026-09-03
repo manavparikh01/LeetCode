bdc1d24734152b20141f3772c741e3ad2d5efcbb
Manav Parikh <manavparikh01@gmail.com>
2026-09-03 00:07:29 -0400

    Time: 929 ms (52.38%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,21 +1,9 @@
 # Write your MySQL query statement below
-# SELECT turn, person_id, person_name, weight, (SELECT SUM(weight)
-#                                              FROM Queue AS q1
-#                                               JOIN Queue AS q2
-#                                               ON q1.turn = q2.turn
-#                                              WHERE q2.turn < q1.turn)
-# FROM Queue
-# ORDER BY turn;
-
-# SELECT SUM(weight) AS total_weight
-# FROM Queue
-# WHERE turn = 1 or turn = 2
-# ORDER BY turn;
-
-WITH Cumulative AS (SELECT *, SUM(Weight) OVER (ORDER BY Turn) AS TotalWeight
-  FROM Queue)
-SELECT person_name
-FROM Cumulative
-WHERE TotalWeight <= 1000
-ORDER BY turn DESC
-LIMIT 1;
+WITH cumulative AS (SELECT *, SUM(weight) OVER (ORDER BY turn) AS cum_sum
+FROM Queue
+ORDER BY turn)
+-- SELECT * FROM cumulative
+SELECT person_name FROM cumulative
+WHERE cum_sum <= 1000
+ORDER BY cum_sum DESC
+LIMIT 1
